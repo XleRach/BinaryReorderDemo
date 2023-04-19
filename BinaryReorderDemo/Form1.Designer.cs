7945fd49a7f5d4b90f9f0dc4e3b75d318b448519
eymen <eymen@DESKTOP-6QJSB6P>
2023-04-19 12:35:50 +0300

    Add project files.

---
+++ BinaryReorderDemo/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace BinaryReorderDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSaveIntoTextFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(135, 91);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(13, 13);
+            this.lblFileName.TabIndex = 12;
+            this.lblFileName.Text = "--";
+            this.toolTip1.SetToolTip(this.lblFileName, "Click to see full file name");
+            this.lblFileName.Click += new System.EventHandler(this.lblFileName_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.btnSaveIntoTextFile);
+            this.panel2.Controls.Add(this.btnSaveFile);
+            this.panel2.Controls.Add(this.btnOpenFile);
+            this.panel2.ForeColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(0, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(128, 178);
+            this.panel2.TabIndex = 11;
+            // 
+            // btnSaveIntoTextFile
+            // 
+            this.btnSaveIntoTextFile.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveIntoTextFile.Location = new System.Drawing.Point(12, 130);
+            this.btnSaveIntoTextFile.Name = "btnSaveIntoTextFile";
+            this.btnSaveIntoTextFile.Size = new System.Drawing.Size(111, 23);
+            this.btnSaveIntoTextFile.TabIndex = 6;
+            this.btnSaveIntoTextFile.Text = "Save into Text File";
+            this.toolTip1.SetToolTip(this.btnSaveIntoTextFile, "Save into text file the reordered binary");
+            this.btnSaveIntoTextFile.UseVisualStyleBackColor = true;
+            this.btnSaveIntoTextFile.Click += new System.EventHandler(this.btnSaveIntoTextFile_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveFile.Location = new System.Drawing.Point(12, 80);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(111, 23);
+            this.btnSaveFile.TabIndex = 5;
+            this.btnSaveFile.Text = "Save File";
+            this.toolTip1.SetToolTip(this.btnSaveFile, "Save the file you reorderd");
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 30);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(111, 23);
+            this.btnOpenFile.TabIndex = 4;
+            this.btnOpenFile.Text = "Open File";
+            this.toolTip1.SetToolTip(this.btnOpenFile, "Opens the file and automatically reorders the binary in reverse ");
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(367, 51);
+            this.panel1.TabIndex = 10;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Image = global::BinaryReorderDemo.Properties.Resources.exit;
+            this.btnExit.Location = new System.Drawing.Point(315, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(40, 40);
+            this.btnExit.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.btnExit, "Exit");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Binary Reorder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 229);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSaveIntoTextFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ BinaryReorderDemo/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryReorderDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFileName.Text = String.Empty;
+        }
+
+        string fileName;
+        byte[] fileBytes;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            RefreshApp();
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFile.FileName;
+                lblFileName.Text = fileName;
+
+                fileBytes = File.ReadAllBytes(fileName);
+                Array.Reverse(fileBytes, 0, fileBytes.Length);
+
+                MessageBox.Show("Use save buttons If you want the file to be saved.", "Reorder is succeed");
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you want to save reordered binary into an existing file?", "Save into an existing file", MessageBoxButtons.YesNo);
+            if (fileName != null && dialogResult == DialogResult.Yes)
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(new FileStream(fileName, FileMode.Truncate, FileAccess.Write)))
+                {
+                    binaryWriter.Write(fileBytes);
+                    //binaryWriter.Flush();
+                    MessageBox.Show("Saved!");
+                    RefreshApp();
+                }
+            }
+            else if (fileName == null && dialogResult == DialogResult.Yes)
+            {
+                MessageBox.Show("Choose file first!", "Exception");
+            }
+        }
+        private void btnSaveIntoTextFile_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you want to save into a text file the reordered binary?", "Save into a new file", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes && fileBytes != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.InitialDirectory = @"C:\txt";
+                saveFileDialog.Filter = "Text Files Only (*.txt) | *.txt";
+                saveFileDialog.DefaultExt = "txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (Stream stream = File.Open(saveFileDialog.FileName, FileMode.CreateNew))
+                    using (StreamWriter streamWriter = new StreamWriter(stream))
+                    {
+                        foreach (byte b in fileBytes)
+                        {
+                            stringBuilder.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+                        }
+                        streamWriter.Write(stringBuilder.ToString());
+                        MessageBox.Show("Saved!");
+                        RefreshApp();
+                    }
+                }
+            }
+            else if (dialogResult == DialogResult.Yes && fileBytes == null)
+            {
+                MessageBox.Show("Choose file first!", "Exception");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void RefreshApp()
+        {
+            lblFileName.Text = String.Empty;
+            fileName = null;
+            fileBytes = null;
+            stringBuilder = new StringBuilder();
+
+        }
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void lblFileName_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(fileName, "Reordered file");
+        }
+    }
+}
